4562565208b8aa1b6e1fb952afbf9075cdb13857
KatieAJenkins <kind2karma@gmail.com>
2017-05-24 17:33:32 -0600

    added \ path and confirmed sending response to browser

--- server.js
+++ server.js
@@ -3,6 +3,11 @@
 var express = require('express');
 var app = express();
 
+app.get("/", function(req, res) {
+  res.send("Hello from API")
+})
+
+
 app.listen('3000', function(){
-  console.log("Listening on port 3000");;
+  console.log("Listening on port 3000");
 });
