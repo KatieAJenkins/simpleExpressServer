f5502269aa2d5e9b16e0b8138cfe1a397f4bed27
KatieAJenkins <kind2karma@gmail.com>
2017-05-24 17:45:12 -0600

    added 404 not found to catch errors

--- server.js
+++ server.js
@@ -3,12 +3,15 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public')); 
+app.use(express.static('public'));
 
 app.get("/api", function(req, res) {
   res.send("Hello from API")
 })
 
+app.use(function(req, res, next){
+  res.status(404).send("Not Found");
+});
 
 app.listen('3000', function(){
   console.log("Listening on port 3000");
